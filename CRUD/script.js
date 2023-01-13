55f78bc0f3fe2551e6746455207155470f448638
Mateus Nunes <116748901+mateusnunes11@users.noreply.github.com>
2023-01-13 19:54:24 -0300

    Initial commit

---
+++ CRUD/script.js
@@ -0,0 +1,98 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sPosicao = document.querySelector('#m-posicao')
+const sTime = document.querySelector('#m-time')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+    modal.classList.add('active')
+  
+    modal.onclick = e => {
+      if (e.target.className.indexOf('modal-container') !== -1) {
+        modal.classList.remove('active')
+      }
+    }
+  
+    if (edit) {
+      sNome.value = itens[index].nome
+      sPosicao.value = itens[index].posicao
+      sTime.value = itens[index].time
+
+    } else {
+      sNome.value = ''
+      sPosicao.value = ''
+      sTime.value = ''
+    }
+    
+  }
+  
+  function editItem(index) {
+  
+    openModal(true, index)
+  }
+  
+  function deleteItem(index) {
+    itens.splice(index, 1)
+    setItensBD()
+    loadItens()
+  }
+  
+  function insertItem(item, index) {
+    let tr = document.createElement('tr')
+  
+    tr.innerHTML = `
+      <td>${item.nome}</td>
+      <td>${item.posicao}</td>
+      <td>${item.time}</td>
+      <td class="acao">
+        <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+      </td>
+      <td class="acao">
+        <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+      </td>
+    `
+    tbody.appendChild(tr)
+  }
+  
+  btnSalvar.onclick = e => {
+    
+    if (sNome.value == '' || sPosicao.value == '' || sTime.value == '') {
+      return
+    }
+  
+    e.preventDefault();
+  
+    if (id !== undefined) {
+      itens[id].nome = sNome.value
+      itens[id].posicao = sPosicao.value
+      itens[id].time = sTime.value
+    } else {
+      itens.push({'nome': sNome.value, 'posicao': sPosicao.value, 'time': sTime.value})
+    }
+  
+    setItensBD()
+  
+    modal.classList.remove('active')
+    loadItens()
+    id = undefined
+  }
+  
+  function loadItens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+      insertItem(item, index)
+    })
+  
+  }
+  
+  const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+  const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+  
+  loadItens()
+
+    
